805375ee35c96183dc33c1ffb4b789ed32a08bb5
mile <791144831@qq.com>
2020-12-16 12:36:53 +0800

    非文本数据的数据slot-scope

---
+++ src/components/users.vue
@@ -0,0 +1,122 @@
+<template>
+  <!-- el-card小容器 可以理解为div 卡片 -->
+  <el-card class="box">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索框 -->
+    <el-row class="searchBox">
+      <el-col>
+        <el-input class="searchInput" placeholder="请输入内容" v-model="query">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="primary">添加按钮</el-button>
+      </el-col>
+    </el-row>
+    <!-- 表格 -->
+    <!-- tableData:[{data:?,name:?,address:?}]
+    el-table-column 控制的列
+      label 控制的是表头
+      prop的值 控制的是该列中每一行单元格的内容
+    -->
+    <el-table :data="list" style="width: 100%">
+      <el-table-column prop="id" label="#" width="80"></el-table-column>
+      <el-table-column prop="username" label="姓名" width="100"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
+      <el-table-column prop="mobile" label="电话" width="140"></el-table-column>
+      <!-- 过滤器fmtdata
+        1. v-bind 后面
+        2{{msg | 名字}}
+      -->
+      <!-- 1.思路 直接给prop赋值 不行-->
+      <!-- <el-table-column prop="create_time | fmtdate" label="创建日期" width="200"></el-table-column> -->
+      <!-- 2.思路 可以实现效果 但是error -->
+      <!-- <el-table-column prop="create_time" label="创建日期" width="200">
+        {{create_time | fmtdate}}
+      </el-table-column> -->
+      <!-- 3.单元格内容不是prop的值，希望{{}}的结果->
+        如果单元格内容不是prop的值，需要给单元格内容的外层加template,
+        此时,外层数据和内层数据有联系吗？没联系
+        外层的list.对象.create_time
+        template 有一个属性slot-scope="外层数据"
+      -->
+      <!-- 3.总结：
+        前提：单元格的内容不是prop的值，
+        3.1 给单元格内容外层加template
+        3.2 给template设置slot-scope
+        3.3 slot-scope的值自动绑定为外层数据el-table ：data="list"
+        3.4 在template内部通过list.row 自动取出数组list中每个对象
+        3.5 list.row.属性create_time
+        注意：row是固定写法
+      -->
+      <el-table-column prop="create_time" label="创建日期" width="200">
+        <template slot-scope="list">
+          <!-- 内层list.row 表示的是list的每个对象 -->
+          {{ list.row.create_time | fmtdate}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="date" label="用户状态" width="120"></el-table-column>
+      <el-table-column prop="date" label="操作" width="200"></el-table-column>
+    </el-table>
+    <!-- 分页 -->
+  </el-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 搜索时的关键字
+      query: '',
+      // 分页功能 -> 前提接口必须支持分页->通常在接口url参数中类似page的参数名
+      pagenum: 1,
+      pagesize: 10,
+      // 表格数据
+      list: []
+    }
+  },
+  // 获取首屏数据写在created中，也可以写在mounted中
+  created () {
+    this.getTableData()
+  },
+  methods: {
+    async getTableData () {
+      // this.$http
+      // 模板字符串`${}`
+      // const per = {name: 'abc'}
+      // `${name}`  -> abc
+      // 请求头
+      // {
+      //   // contentType: 'text/html';
+      //   // Authorization: ? 正确用户的token
+      // }
+
+      // 设置请求头？
+      // 设置发送请求时的请求头 -> axios库 -> 找axios中有没有可以设置headers头部的API -> 看axios文档
+      // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+      const AUTH_TOKEN = localStorage.getItem('token')
+      console.log(AUTH_TOKEN)
+      this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
+      const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
+      console.log(res)
+      const {data, meta: {msg, status}} = res.data
+      if (status === 200) {
+        this.list = data.users
+        console.log(this.list)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.searchBox {
+  margin-top: 20px;
+}
+.searchInput {
+  width: 400px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,20 @@ import Router from 'vue-router'
 // webpack项目模板 提供@表示src文件夹->自动找到src文件夹
 import Login from '@/components/login.vue'
 import Home from '@/components/home.vue'
+import Users from '@/components/users.vue'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     name: 'home',
     path: '/',
-    component: Home
+    component: Home,
+    children: [{
+      name: 'users',
+      path: '/users',
+      component: Users
+    }]
   }, {
     name: 'login',
     path: '/login',
