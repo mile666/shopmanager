24a5a8a932c2bb2b2049285625fb4e4ff3afd03f
mile <791144831@qq.com>
2020-12-21 23:12:05 +0800

    表格展开-三级权限

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -4,6 +4,28 @@
     <el-button class="btn" type="primary">添加角色</el-button>
     <!-- 表格 -->
     <el-table :data="roles" style="width: 100%">
+      <el-table-column type="expand">
+        <template slot-scope="scope">
+          <!-- 写展示的内容 -->
+          <el-row class="level1" v-for="(item1, i) in scope.row.children" :key="item1.id">
+            <el-col :span="4">
+              <el-tag closable type="danger">{{item1.authName}}</el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row class="level2" v-for="(item2, i) in item1.children" :key="item2.id">
+                <el-col :span="4">
+                  <el-tag closable type="info">{{item2.authName}}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag closable v-for="(item3, i) in item2.children" :key="item3.id" type="warning">{{item3.authName}}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
       <el-table-column type="index" label="#" width="160"></el-table-column>
       <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
       <el-table-column prop="roleDesc" label="角色描述" width="200"></el-table-column>
@@ -49,4 +71,8 @@ export default {
   .btn {
     margin-top: 20px;
   }
+  .level1 .level2{
+    margin-bottom: 10px;
+  }
+  
 </style>
